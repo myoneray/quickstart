e8e65d63636de01810ec5e761f047fb7eefe87ec
wangming <wangm@unionpaysmart.com>
2016-08-05 18:10:05 +0800

    指定编译后的输出目录 未成功：amd & system 不支持

--- app/hero.ts
+++ app/hero.ts
@@ -1,8 +1,7 @@
-import { bootstrap } from '@angular/platform-browser-dynamic';
-
-import { AppComponent } from './app.component';
-
-bootstrap(AppComponent);
+export class Hero {
+  id: number;
+  name: string;
+}
 
 
 /*
